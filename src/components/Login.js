ccc1683d1c12c83f464b4af50f9a3481bb77afe2
ZeenatFirdosh <zeenatzfq052@gmail.com>
2023-05-13 01:44:56 +0530

    backend deployed link added

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = ({showAlert}) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/auth/login", {
+        const response = await fetch("https://precious-lime-wetsuit.cyclic.app/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
